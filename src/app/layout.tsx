d4228770ba8cf88e23ba73bdfdbc4de079eb5205
Ninoverstraeten2003 <r0898606@ucll.be>
2024-01-03 00:38:27 +0100

    add: analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
       <head />
       <body className={cn(inter.className, "h-full")}>
         <main className="h-full">
-          <Providers>{children}</Providers>
+          <Providers>
+            {children}
+            <Analytics />
+          </Providers>
         </main>
       </body>
     </html>
