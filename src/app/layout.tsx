fce98c5972ac3f626b6d20263e463a01bd8a463e
Ninoverstraeten2003 <r0898606@ucll.be>
2024-01-03 00:53:19 +0100

    add: speedinsights vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Providers>
             {children}
             <Analytics />
+            <SpeedInsights />
           </Providers>
         </main>
       </body>
