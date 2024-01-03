6a2373a477dd600d1d26484246ba6e075242388b
Ninoverstraeten2003 <r0898606@ucll.be>
2024-01-03 22:48:17 +0100

    update: scopes

--- src/app/api/auth/[...nextauth]/spotify-profile.ts
+++ src/app/api/auth/[...nextauth]/spotify-profile.ts
@@ -13,19 +13,7 @@ const scopes = [
   "user-read-email",
   "playlist-read-private",
   "playlist-read-collaborative",
-  "user-read-email",
-  "streaming",
-  "user-read-private",
   "user-library-read",
-  "user-top-read",
-  "app-remote-control",
-  "streaming",
-  "user-read-playback-position",
-  "user-top-read",
-  "user-read-recently-played",
-  "user-read-playback-state",
-  "user-modify-playback-state",
-  "user-read-currently-playing",
 ];
 
 authURL.searchParams.append("scope", scopes.join(" "));
