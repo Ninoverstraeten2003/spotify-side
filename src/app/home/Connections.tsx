f4553b8a5d01103e0d10e3acb1a6a1c4381c02c5
Ninoverstraeten2003 <r0898606@ucll.be>
2024-01-05 00:59:44 +0100

    add: likes and user suggestions

---
+++ src/app/home/Connections.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { User } from "@spotify/web-api-ts-sdk";
+import { ChevronsUpDown, MoveRight } from "lucide-react";
+import React from "react";
+
+type ConvertMapArrayToMap<ArrayType extends Array<{ [Key: string | number | symbol]: any }>, KeyType extends keyof ArrayType[number], ValueType extends keyof ArrayType[number]> = {
+  [K in ArrayType[number][KeyType]]: ArrayType[number][ValueType];
+};
+
+const convertMapArrayToMap = <ItemType extends Record<KeyType | ValueType, any>, KeyType extends keyof ItemType, ValueType extends keyof ItemType>(mapArray: ItemType[], key: KeyType, valueKey: ValueType): ConvertMapArrayToMap<ItemType[], KeyType, ValueType> => {
+  return mapArray.reduce(
+    (accumulator, currentItem) => {
+      accumulator[currentItem[key]] = currentItem[valueKey];
+      return accumulator;
+    },
+    {} as ConvertMapArrayToMap<ItemType[], KeyType, ValueType>
+  );
+};
+
+export default function Connections({ connections }: { connections: User[] }) {
+  const [open, setOpen] = React.useState(false);
+  const [open2, setOpen2] = React.useState(false);
+
+  const [leftValues, setLeftValues] = React.useState<ConvertMapArrayToMap<User[], "display_name", "id"> | null>(convertMapArrayToMap(connections, "display_name", "id"));
+  const [rightValues, setRightValues] = React.useState<ConvertMapArrayToMap<User[], "display_name", "id"> | null>(null);
+
+  return (
+    <div className="flex flex-wrap items-center gap-2">
+      <Popover
+        open={open}
+        onOpenChange={(open) => {
+          setOpen(open);
+        }}
+      >
+        <PopoverTrigger asChild>
+          <Button variant="outline" role="combobox" aria-expanded={open} className="mt-0 w-[200px] justify-between">
+            {"Add Connection"}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search Connections" disabled={leftValues === null} />
+            <CommandEmpty className={cn({ hidden: !leftValues, "py-6 text-center text-sm": leftValues })}>No connection found.</CommandEmpty>
+            <CommandGroup>
+              <ScrollArea className="">
+                {leftValues === null ? (
+                  <div className="py-6 text-center text-sm">No connections.</div>
+                ) : (
+                  Object.entries(leftValues)
+                    .sort(([name1, _], [name2, __]) => (name1 < name2 ? -1 : name1 > name2 ? 1 : 0))
+                    .map(([name, id]) => (
+                      <CommandItem
+                        key={id}
+                        value={name}
+                        onSelect={() => {
+                          delete leftValues[name];
+                          if (Object.keys(leftValues).length === 0) setLeftValues(null);
+                          setRightValues({ ...rightValues, [name]: id });
+                          setOpen(false);
+                        }}
+                      >
+                        {name}
+                      </CommandItem>
+                    ))
+                )}
+              </ScrollArea>
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      <MoveRight className="hidden sm:inline-block" />
+      <Popover
+        open={open2}
+        onOpenChange={(open2) => {
+          setOpen2(open2);
+        }}
+      >
+        <PopoverTrigger asChild>
+          <Button variant="outline" role="combobox" aria-expanded={open2} className="mt-0 w-[200px] justify-between">
+            {"Added Connections"}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search Connections" disabled={rightValues === null} />
+            <CommandEmpty className={cn({ hidden: !rightValues, "py-6 text-center text-sm": rightValues })}>No connection found.</CommandEmpty>
+            <CommandGroup>
+              <ScrollArea>
+                {rightValues === null ? (
+                  <div className="py-6 text-center text-sm">No connections added.</div>
+                ) : (
+                  Object.entries(rightValues)
+                    .sort(([name1, _], [name2, __]) => (name1 < name2 ? -1 : name1 > name2 ? 1 : 0))
+                    .map(([name, id]) => (
+                      <CommandItem
+                        key={id}
+                        value={name}
+                        onSelect={() => {
+                          delete rightValues[name];
+                          if (Object.keys(rightValues).length === 0) setRightValues(null);
+                          setLeftValues({ ...leftValues, [name]: id });
+                          setOpen2(false);
+                        }}
+                      >
+                        {name}
+                      </CommandItem>
+                    ))
+                )}
+              </ScrollArea>
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
--- src/app/home/[playlistId]/page.tsx
+++ src/app/home/[playlistId]/page.tsx
@@ -1,45 +1,22 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import keys from "@/keys";
-import {
-  AccessToken,
-  SpotifyApi,
-  Playlist as SpotifyPlaylist,
-} from "@spotify/web-api-ts-sdk";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getServerSession } from "next-auth/next";
+import Image from "next/image";
 import authOptions from "../../api/auth/[...nextauth]/auth-options";
-
-const getPlaylist = async ({
-  accessToken,
-  playlistId,
-}: {
-  accessToken: AccessToken;
-  playlistId: string;
-}) => {
-  try {
-    const playlist: SpotifyPlaylist = await SpotifyApi.withAccessToken(
-      keys.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-      accessToken
-    ).playlists.getPlaylist(playlistId);
-
-    return playlist;
-  } catch (e) {
-    return { type: "error", message: (e as Error).message };
-  }
-};
-
-const isPlaylist = (value: any): value is SpotifyPlaylist =>
-  value.type === "error" ? false : true;
+import { getPlaylist, getPossibleConnections, getLikedTracks, isPlaylist } from "@/service/index";
+import Connections from "../Connections";
+import { Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const PlaylistPage = async ({ params }: { params: { playlistId: string } }) => {
   const session = await getServerSession(authOptions);
+  const accessToken = {
+    access_token: session?.user.access_token!,
+    expires_in: session?.user.expires_in!,
+    token_type: session?.user.token_type!,
+    refresh_token: session?.user.refresh_token!,
+  };
+
   const playlist = await getPlaylist({
     playlistId: params.playlistId!,
     accessToken: {
@@ -51,27 +28,32 @@ const PlaylistPage = async ({ params }: { params: { playlistId: string } }) => {
   });
   if (!isPlaylist(playlist)) return <p>{playlist.message}</p>;
 
+  const connections = await getPossibleConnections({
+    userId: session?.user.id!,
+    accessToken: accessToken,
+  });
+
+  const likedTrackIds = await getLikedTracks({ trackIds: playlist.tracks.items.map((track) => track.track.id), accessToken: accessToken });
+  
+  // const likedTrackIds = new Set<string>();
+
   return (
     <>
-      <div className="container mx-auto p-6">
+      <div className="container mx-auto mt-10 px-10">
         <Card>
           <CardHeader className="mx-4 flex flex-col gap-2">
-            <h2 className="text-2xl font-bold text-gray-800">
-              {playlist.name}
-            </h2>
-            <p className="text-sm text-gray-500">{playlist.description}</p>
-            <div className="flex items-center gap-2">
-              <span className="text-sm text-gray-500">Collaborative:</span>
-              <span className="text-sm text-gray-800">
-                {playlist.collaborative ? "Yes" : "No"}
-              </span>
-            </div>
-            <div className="flex items-center gap-2">
-              <span className="text-sm text-gray-500">Owner:</span>
-              <span className="text-sm text-gray-800">
-                {playlist.owner.display_name}
-              </span>
-            </div>
+            <CardTitle className="flex items-center gap-4">
+              <Image className="h-10 w-10 rounded-md object-cover" src={playlist.images[0].url} alt={playlist.name} width={40} height={40} />
+              <div>
+                <h2 className="text-2xl font-bold text-gray-800">{playlist.name}</h2>
+                <p className="text-sm text-gray-500">{playlist.description}</p>
+              </div>
+            </CardTitle>
+            <CardDescription className="flex flex-wrap items-center gap-2">
+              <span className="mr-2 text-sm text-gray-500">Owner:</span>
+              <span className="text-sm text-gray-800">{playlist.owner.display_name}</span>
+            </CardDescription>
+            <Connections connections={connections} />
           </CardHeader>
           <CardContent>
             <Table>
@@ -79,19 +61,17 @@ const PlaylistPage = async ({ params }: { params: { playlistId: string } }) => {
                 <TableRow className="text-sm font-medium text-gray-500">
                   <TableHead>Track Name</TableHead>
                   <TableHead>Duration</TableHead>
+                  <TableHead>Liked</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 {playlist.tracks.items.map((track) =>
                   track.track !== null ? (
-                    <TableRow
-                      key={track.track.id}
-                      className="text-sm text-gray-800"
-                    >
+                    <TableRow key={track.track.id} className="text-sm text-gray-800">
                       <TableCell>{track.track.name}</TableCell>
+                      <TableCell className="text-nowrap">{Number(track.track.duration_ms / 1000 / 60).toFixed(2)} min</TableCell>
                       <TableCell>
-                        {Number(track.track.duration_ms / 1000 / 60).toFixed(2)}{" "}
-                        min
+                        <Heart className={cn(likedTrackIds.has(track.track.id) && "fill-red-500", "text-red-500")} />
                       </TableCell>
                     </TableRow>
                   ) : null
